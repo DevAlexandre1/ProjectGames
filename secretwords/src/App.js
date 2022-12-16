8c8aaccb8053fb5b91266e780403bfbe784038d0
Alexandre Henrique <alexandre_eaw@hotmail.com>
2022-12-16 14:25:46 -0300

    Update App.js

--- secretwords/src/App.js
+++ secretwords/src/App.js
@@ -25,8 +25,30 @@ function App() {
   const [gameStage, setGameStage]= useState(stages[0].name);
   const [words]= useState(wordsList);
 
+  const [piickedWord,setPickdWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory=()=>{
+    //Pegando uma categoria aleatoria
+    const categories = Object.keys(words)
+    //math.random  da um numero aleatorio entre 0 e o tamanho da categoria
+    //math.floor vai arredondar o numero random para baixo
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+    //Pegando uma palavra aleatoria
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+
+  }
+
 
     const startGame = ()=>{
+
+      //pega uma palavra e uma categoria aleatoria
+      pickWordAndCategory()
       setGameStage(stages[1].name)
     }
 
